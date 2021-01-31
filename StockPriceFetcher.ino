365c06e85edfeaeaf40927b0762e140a49e0c412
Lasse Lukkari <lasse.lukkari@gmail.com>
2021-01-31 15:06:11 +0200

    Print more values from response

--- StockPriceFetcher.ino
+++ StockPriceFetcher.ino
@@ -9,8 +9,10 @@
 #define STOCK_SYMBOL "GME"
 #define INTERVAL 300000
 
+
 HTTPClient https;
 std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+StaticJsonDocument<1000> data;
 
 void makeRequest() {
   if (!https.begin(*client, "https://financialmodelingprep.com/api/v3/quote/" STOCK_SYMBOL "?apikey=" APIKEY)) {
@@ -18,24 +20,40 @@ void makeRequest() {
     return;
   }
 
-  if ( https.GET() != HTTP_CODE_OK) {
-    Serial.println("API request failed");
+  int status = https.GET();
+  if (status != HTTP_CODE_OK) {
+    Serial.print("API request failed: ");
+    Serial.println(status);
     return;
   }
 
-  StaticJsonDocument<1000> doc;
-  DeserializationError error = deserializeJson(doc, https.getString());
-  if (error) {
-    Serial.println("Failed to deserialize JSON");
-    return;
-  }
-
-  // serializeJsonPretty(doc, Serial);
-
-  float price = doc[0]["price"];
-  Serial.println(price);
-
+  String response = https.getString();
   https.end();
+
+  DeserializationError error = deserializeJson(data, response);
+  if (error) {
+    Serial.println("Failed to deseiralice JSON:");
+    Serial.println(response);
+    return;
+  }
+
+  Serial.println("Response: ");
+  serializeJsonPretty(data, Serial);
+  Serial.println();
+
+  float price = data[0]["price"];
+  float open = data[0]["open"];
+  float dayLow = data[0]["dayLow"];
+  float dayHigh = data[0]["dayHigh"];
+
+  Serial.print("Price: ");
+  Serial.println(price);
+  Serial.print("Open: ");
+  Serial.println(open);
+  Serial.print("Low: ");
+  Serial.println(dayLow);
+  Serial.print("High: ");
+  Serial.println(dayHigh);
 }
 
 void setup() {
